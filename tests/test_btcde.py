db14a98bad1f5d034081413a5090b783df70f88a
peshay <peshay@users.noreply.github.com>
2017-08-20 18:54:47 +0200

    testy

--- tests/test_btcde.py
+++ tests/test_btcde.py
@@ -15,7 +15,7 @@ class TestBtcdeApi(TestCase):
     @patch('btcde.APIConnect')
     def test_showOrderbook_buy(self, mock_APIConnect):
         result = btcde.showOrderbook('mock', 'buy')
-        expected_arguments = ['mock', 'GET', {'type': 'buy'}, btcde.orderuri]
+        expected_arguments = ('mock', 'GET', {'type': 'buy'}, btcde.orderuri)
         for callargs in mock_APIConnect.call_args[0]:
             print(callargs)
         self.assertEqual(mock_APIConnect.call_args[0], expected_arguments)
