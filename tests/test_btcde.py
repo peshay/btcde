157bbf04dadd0414a5c808a6a4895b5ac14e83f2
peshay <peshay@users.noreply.github.com>
2017-08-20 20:37:15 +0200

    just the other way arround

--- tests/test_btcde.py
+++ tests/test_btcde.py
@@ -16,8 +16,8 @@ class TestBtcdeApi(TestCase):
     def test_showOrderbook_buy(self, mock_APIConnect):
         result = btcde.showOrderbook('mock', 'buy')
         expected_arguments = ('mock', 'GET', {'type': 'buy'}, btcde.orderuri)
-        for idx, actual in enumerate(mock_APIConnect.call_args[0]):
-            expected = expected_arguments[idx]
+        for idx, expected in enumerate(expected_arguments):
+            actual = mock_APIConnect.call_args[0][idx]
             self.assertEqual(actual, expected,
                              'Argument {} with value {} '
                              'does not match expected {}'.format(idx,
