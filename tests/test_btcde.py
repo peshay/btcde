abdeab5e9a38c77eac1f43cde6b2a9d84b1c554e
peshay <peshay@users.noreply.github.com>
2017-08-21 22:02:34 +0200

    two new tests

--- tests/test_btcde.py
+++ tests/test_btcde.py
@@ -13,13 +13,41 @@ class TestBtcdeApi(TestCase):
     def tearDown(self):
         self.patcher.stop()
     
-    def test_showOrderbook_buy(self):
-        result = btcde.showOrderbook('mock', 'buy')
-        expected_arguments = ['mock', 'GET', {'type': 'buy'}, btcde.orderuri]
-        for idx, expected in enumerate(expected_arguments):
-            actual = self.mock_APIConnect.call_args[0][idx]
-            self.assertEqual(actual, expected,
-                             'Argument {} with value {} '
-                             'does not match expected {}'.format(idx,
-                                                                 actual,
-                                                                 expected))
+    def test_showOrderbook_buy_and_sell(self):
+        methods = 'buy', 'sell'
+        for method in method:
+            result = btcde.showOrderbook('mock', method)
+            expected_arguments = ['mock', 'GET', {'type': method}, btcde.orderuri]
+            for idx, expected in enumerate(expected_arguments):
+                actual = mock_APIConnect.call_args[0][idx]
+                self.assertEqual(actual, expected,
+                                 'Argument {} with value {} '
+                                 'does not match expected {}'.format(idx,
+                                                                     actual,
+                                                                     expected))
+    
+    def test_createOrder(self):
+        OrderType = 'now'
+        max_amount = 5
+        price = 10
+        result = btcde.createOrder('mock', OrderType, max_amount, price)
+        params = {'type': OrderType, 'max_amount': max_amount, 'price': price}
+        expected_arguments = ['mock', 'POST', params, btcde.orderuri]
+        actual = mock_APIConnect.call_args[0][idx]
+        self.assertEqual(actual, expected,
+                         'Argument {} with value {} '
+                         'does not match expected {}'.format(idx,
+                                                             actual,
+                                                             expected))
+
+    def test_deleteOrder(self):
+        order_id = 42
+        result = btcde.deleteOrder('mock', order_id)
+        params = {'order_id': order_id}
+        expected_arguments = ['mock', 'DELETE', params, btcde.orderuri + "/" + order_id]
+        actual = mock_APIConnect.call_args[0][idx]
+        self.assertEqual(actual, expected,
+                         'Argument {} with value {} '
+                         'does not match expected {}'.format(idx,
+                                                             actual,
+                                                             expected))
