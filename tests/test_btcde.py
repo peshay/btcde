118f7dc14aea29445e4ef00f01d8fd2a756d1ec9
peshay <peshay@users.noreply.github.com>
2017-08-21 22:32:04 +0200

    fix last tests

--- tests/test_btcde.py
+++ tests/test_btcde.py
@@ -15,7 +15,7 @@ class TestBtcdeApi(TestCase):
     
     def test_showOrderbook_buy_and_sell(self):
         methods = 'buy', 'sell'
-        for method in method:
+        for method in methods:
             result = btcde.showOrderbook('mock', method)
             expected_arguments = ['mock', 'GET', {'type': method}, btcde.orderuri]
             for idx, expected in enumerate(expected_arguments):
@@ -33,7 +33,7 @@ class TestBtcdeApi(TestCase):
         result = btcde.createOrder('mock', OrderType, max_amount, price)
         params = {'type': OrderType, 'max_amount': max_amount, 'price': price}
         expected_arguments = ['mock', 'POST', params, btcde.orderuri]
-        actual = self.mock_APIConnect.call_args[0][idx]
+        actual = self.mock_APIConnect.call_args[0][0]
         self.assertEqual(actual, expected,
                          'Argument {} with value {} '
                          'does not match expected {}'.format(idx,
@@ -41,11 +41,11 @@ class TestBtcdeApi(TestCase):
                                                              expected))
 
     def test_deleteOrder(self):
-        order_id = 42
+        order_id = '42'
         result = btcde.deleteOrder('mock', order_id)
         params = {'order_id': order_id}
         expected_arguments = ['mock', 'DELETE', params, btcde.orderuri + "/" + order_id]
-        actual = self.mock_APIConnect.call_args[0][idx]
+        actual = self.mock_APIConnect.call_args[0][0]
         self.assertEqual(actual, expected,
                          'Argument {} with value {} '
                          'does not match expected {}'.format(idx,
