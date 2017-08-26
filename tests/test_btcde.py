f1a562c31ce48e22555a46716477b21f4fe274fc
peshay <peshay@users.noreply.github.com>
2017-08-26 10:55:03 +0200

    test all standard functions

--- tests/test_btcde.py
+++ tests/test_btcde.py
@@ -1,205 +1,115 @@
-#! /usr/bin/env python
-"""This Module provides the functions from bitcoin.de Trading API"""
-
-import requests
-import time
-import json
-import hmac
-import hashlib
-import logging
-import codecs
-
-# these two lines enable debugging at httplib level
-# (requests->urllib3->httplib)
-# you will see the REQUEST, including HEADERS and DATA, and RESPONSE with
-# HEADERS but without DATA.
-# the only thing missing will be the response.body which is not logged.
-import http.client
-http.client.HTTPConnection.debuglevel = 1
+from unittest import TestCase 
+from mock import patch
+import btcde
 
 
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
+class TestBtcdeApi(TestCase):
+    """Test Api Functions."""
+    
+    def setUp(self):
+        self.patcher = patch('btcde.APIConnect')
+        self.mock_APIConnect = self.patcher.start()
+    
+    def tearDown(self):
+        self.patcher.stop()
+    
+    def assertArguments(expected_arguments, mock_APIConnect):
+        for idx, expected in enumerate(expected_arguments):
+            actual = mock_APIConnect.call_args[0][idx]
+            self.assertEqual(actual, expected,
+                             'Argument {} with value {} '
+                             'does not match expected {}'.format(idx,
+                                                                 actual,
+                                                                 expected))
+    
+    def test_showOrderbook_buy_and_sell(self):
+        methods = 'buy', 'sell'
+        for method in method:
+            result = btcde.showOrderbook('mock', method)
+            expected_arguments = ['mock', 'GET', {'type': method}, btcde.orderuri]
+            self.assertArguments(expected_arguments, self.mock_APIConnect)
+    
+    def test_createOrder(self):
+        OrderType = 'now'
+        max_amount = 5
+        price = 10
+        result = btcde.createOrder('mock', OrderType, max_amount, price)
+        params = {'type': OrderType, 'max_amount': max_amount, 'price': price}
+        expected_arguments = ['mock', 'POST', params, btcde.orderuri]
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
 
-__version__ = '1.4'
+    def test_deleteOrder(self):
+        order_id = '42'
+        result = btcde.deleteOrder('mock', order_id)
+        params = {'order_id': order_id}
+        expected_arguments = ['mock', 'DELETE', params, btcde.orderuri + "/" + order_id]
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
+                                                             
+    def test_showMyOrders(self):
+        result = btcde.showMyOrders('mock')
+        expected_arguments = ['mock', 'GET', btcde.orderuri + '/my_own']
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
 
+    def test_showMyOrderDetails(self):
+        order_id = '42'
+        result = btcde.showMyOrderDetails('mock', order_id)
+        params = {'order_id': order_id}
+        expected_arguments = ['mock', 'GET', btcde.orderuri + '/' + order_id]
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
+                                                             
+    def test_executeTrade(self):
+        order_id = '42'
+        OrderType = 'foobar'
+        amount = '73'
+        result = btcde.executeTrade('mock', order_id, OrderType, amount)
+        params = {'order_id': order_id, 'type': OrderType, 'amount': amount}
+        expected_arguments = ['mock', 'POST', params, btcde.tradeuri + '/' + order_id]
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
 
-class Connection:
-    """To provide connection credentials to the trading API"""
-    def __init__(self, api_key, api_secret):
-        self.api_key = api_key
-        self.api_secret = api_secret
+    def test_showMyTrades(self):
+        result = btcde.showMyTrades('mock')
+        expected_arguments = ['mock', 'GET', btcde.tradeuri]
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
+        
+    def test_showMyTradeDetails(self):
+        trade_id = '42'
+        result = btcde.showMyTradeDetails('mock', trade_id)
+        params = {'trade_id': trade_id}
+        expected_arguments = ['mock', 'GET', btcde.tradeuri + '/' + trade_id]
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
 
-# Bitcoin.de API URI
-apihost = 'https://api.bitcoin.de'
-apiversion = 'v1'
-orderuri = apihost + '/' + apiversion + '/' + 'orders'
-tradeuri = apihost + '/' + apiversion + '/' + 'trades'
-accounturi = apihost + '/' + apiversion + '/' + 'account'
-# set initial nonce
-nonce = int(time.time())
-# disable unsecure SSL warning
-requests.packages.urllib3.disable_warnings()
-
-
-def HandleRequestsException(e):
-    """Handle Exception from request."""
-    print(e[0][0])
-    print(e[0][1])
-
-
-def HandleAPIErrors(r):
-    """To handle Errors from BTCDE API."""
-    if r.status_code != 200 and r.status_code != 201 and r.status_code != 204:
-        reader = codecs.getreader("utf-8")
-        content = json.load(reader(r.raw))
-        errors = content.get('errors')
-        print('Code:     ' + str(errors[0]['code']))
-        print('Message:  ' + errors[0]['message'])
-        print('With URL: ' + r.url)
-        return False
-    else:
-        return True
-
-
-def APIConnect(conn, method, params, uri):
-    """Transform Parameters to URL"""
-    global nonce
-    # set header
-    header = {'content-type':
-              'application/x-www-form-urlencoded; charset=utf-8'}
-    encoded_string = ''
-    if params:
-        for key, value in sorted(params.items()):
-            encoded_string += str(key) + '=' + str(value) + '&'
-        encoded_string = encoded_string[:-1]
-        url = uri + '?' + encoded_string
-    else:
-        url = uri
-    # raise nonce before using
-    nonce += 1
-    if method == 'POST':
-        md5_encoded_query_string = hashlib.md5(encoded_string.encode()).hexdigest()
-    else:
-        md5_encoded_query_string = hashlib.md5(b'').hexdigest()
-    hmac_data = method + '#' + \
-        url + '#' + conn.api_key + \
-        '#' + str(nonce) + '#' + md5_encoded_query_string
-    hmac_signed = hmac.new(bytearray(conn.api_secret.encode()), msg=hmac_data.encode(), digestmod=hashlib.sha256).hexdigest()
-    # set values for header
-    header.update({'X-API-KEY': conn.api_key,
-                   'X-API-NONCE': str(nonce),
-                   'X-API-SIGNATURE': hmac_signed})
-    try:
-        if method == 'GET':
-            r = requests.get(url, headers=(header),
-                             stream=True, verify=False)
-        elif method == 'POST':
-            r = requests.post(url, headers=(header), data=encoded_string,
-                              stream=True, verify=False)
-        elif method == 'DELETE':
-            r = requests.delete(url, headers=(header),
-                                stream=True, verify=False)
-        # Handle API Errors
-        if HandleAPIErrors(r):
-            # get results
-            result = r.json()
-        else:
-            result = {}
-    except requests.exceptions.RequestException as e:
-        HandleRequestsException(e)
-    return result
-
-
-def showOrderbook(conn, OrderType, **args):
-    """Search Orderbook for offers."""
-    # Build parameters
-    if OrderType == 'buy' or OrderType == 'sell':
-        params = {'type': OrderType}
-    else:
-        print('problem')
-    params.update(args)
-    return APIConnect(conn, 'GET', params, orderuri)
-
-
-def createOrder(conn, OrderType, max_amount, price, **args):
-    """Create a new Order."""
-    # Build parameters
-    params = {'type': OrderType, 'max_amount': max_amount, 'price': price}
-    params.update(args)
-    return APIConnect(conn, 'POST', params, orderuri)
-
-
-def deleteOrder(conn, order_id):
-    """Delete an Order."""
-    newuri = orderuri + "/" + order_id
-    params = {'order_id': order_id}
-    return APIConnect(conn, 'DELETE', params, newuri)
-
-
-def showMyOrders(conn, **args):
-    """Query and Filter own Orders."""
-    newuri = orderuri + '/my_own'
-    return APIConnect(conn, 'GET', args, newuri)
-
-
-def showMyOrderDetails(conn, order_id):
-    """Details to an own Order."""
-    newuri = orderuri + '/' + order_id
-    params = {'order_id': order_id}
-    return APIConnect(conn, 'GET', params, newuri)
-
-
-def executeTrade(conn, order_id, OrderType, amount):
-    """Buy/Sell on a specific Order."""
-    newuri = tradeuri + '/' + order_id
-    params = {'order_id': order_id, 'type': OrderType, 'amount': amount}
-    return APIConnect(conn, 'POST', params, newuri)
-
-
-def showMyTrades(conn, **args):
-    """Query and Filter on past Trades."""
-    return APIConnect(conn, 'GET', args, tradeuri)
-
-
-def showMyTradeDetails(conn, trade_id):
-    """Details to a specific Trade."""
-    newuri = tradeuri + '/' + trade_id
-    params = {'trade_id': trade_id}
-    return APIConnect(conn, 'GET', params, newuri)
-
-
-def showAccountInfo(conn):
-    """Query on Account Infos."""
-    params = {}
-    return APIConnect(conn, 'GET', params, accounturi)
-
-
-def showOrderbookCompact(conn):
-    """Bids and Asks in compact format."""
-    params = {}
-    return APIConnect(conn, 'GET', params, orderuri + '/compact')
-
-
-def showPublicTradeHistory(conn, since_tid=None):
-    """All successful trades of the las 7 days."""
-    if since_tid is not None:
-        params = {'since_tid': since_tid}
-    else:
+    def test_showAccountInfo(self):
+        result = btcde.showAccountInfo('mock')
         params = {}
-    return APIConnect(conn, 'GET', params, tradeuri + '/history')
+        expected_arguments = ['mock', 'GET', params, btcde.accounturi]
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
 
+    def test_showOrderbookCompact(self):
+        result = btcde.showOrderbookCompact('mock')
+        params = {}
+        expected_arguments = ['mock', 'GET', params, btcde.orderuri + '/compact']
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
 
-def showRates(conn):
-    """Query of the average rate last 3 and 12 hours."""
-    newuri = apihost + '/' + apiversion + '/rates'
-    params = {}
-    return APIConnect(conn, 'GET', params, newuri)
+    def test_showPublicTradeHistory(self):
+        result1 = btcde.showPublicTradeHistory('mock')
+        params = {}
+        expected_arguments = ['mock', 'GET', params, btcde.tradeuri + '/history']
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
+        self.tearDown()
+        self.setUp()
+        since_tid = '3'
+        params = {'since_tid': since_tid}
+        result2 = btcde.showPublicTradeHistory('mock', since_tid)
+        expected_arguments = ['mock', 'GET', params, btcde.tradeuri + '/history']
+        self.assertArguments(expected_arguments, self.mock_APIConnect) 
 
+    def test_showRates(self):
+        result = btcde.showRates('mock')
+        params = {}
+        expected_arguments = ['mock', 'GET', params, btcde.apihost + '/' + btcde.apiversion + '/rates']
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
 
-def showAccountLedger(conn, **args):
-    """Query on Account statement."""
-    return APIConnect(conn, 'GET', args, accounturi + '/ledger')
+    def test_showAccountLedger(self):
+        result = btcde.showAccountLedger('mock')
+        expected_arguments = ['mock', 'GET', btcde.accounturi + '/ledger']
+        self.assertArguments(expected_arguments, self.mock_APIConnect)
