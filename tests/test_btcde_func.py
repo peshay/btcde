98dc388dc1a820fa5cc25942dd8137c2d6f9cc71
peshay <peshay@users.noreply.github.com>
2017-12-26 12:35:15 +0100

    use response data fom json files
    Instead of having responses inline, they are no read from resources
    directory

--- tests/test_btcde_func.py
+++ tests/test_btcde_func.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 import hashlib
 import hmac
 import requests_mock
+import json
 import btcde
 
 
@@ -9,6 +10,12 @@ import btcde
 class TestBtcdeAPIDocu(TestCase):
     '''Tests are as in bitcoin.de API documentation.
     https://www.bitcoin.de/de/api/tapi/v2/docu'''
+    
+    def sampleData(self, file):
+        '''Retrieve sample data from json files.'''
+        filepath = 'tests/resources/{}.json'.format(file)
+        data = json.load(open(filepath))
+        return data
 
     def sortParams(self, url, params={}):
         '''To sort params for url string.'''
@@ -59,9 +66,7 @@ class TestBtcdeAPIDocu(TestCase):
                   'trading_pair': 'btceur',
                   'max_amount': 10,
                   'price': 1337}
-        response = {"order_id": "A1234BC",
-                    "errors": [],
-                    "credits": 8}
+        response = self.sampleData('createOrder')
         m.post(requests_mock.ANY, json=response, status_code=201)
         self.conn.createOrder(params.get('type'),
                               params.get('trading_pair'),
@@ -78,25 +83,7 @@ class TestBtcdeAPIDocu(TestCase):
         '''Test the signature on a get request.'''
         params = {'type': 'buy',
                   'trading_pair': 'btceur'}
-        response = {"orders":
-                    {"order_id": "A1B2D3",
-                     "trading_pair": "btceur", "type": "buy",
-                     "max_amount": 0.5, "min_amount": 0.1,
-                     "price": 230.55, "max_volume": 115.28,
-                     "min_volume": 23.06,
-                     "order_requirements_fullfilled": True,
-                     "trading_partner_information":
-                     {"username": "bla", "is_kyc_full": True,
-                      "trust_level": "gold", "bank_name": "Sparkasse",
-                      "bic": "HASPDEHHXXX", "seat_of_bank": "DE", "rating": 99,
-                      "amount_trades": 52},
-                     "order_requirements":
-                     {"min_trust_level": "gold",
-                      "only_kyc_full": True,
-                      "seat_of_bank": ["DE", "NL"],
-                      "payment_option": 1, }
-                     }, "errors": [],
-                    "credits": 12}
+        response = self.sampleData('showOrderbook_buy')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showOrderbook(params.get('type'),
                                 params.get('trading_pair'))
@@ -130,42 +117,7 @@ class TestBtcdeAPIDocu(TestCase):
                    .format(params.get('price'),
                            params.get('trading_pair'),
                            params.get('type'))
-        response = {
-           "orders": {
-               "order_id": "A1B2D3",
-               "trading_pair": "btceur",
-               "type": "buy",
-               "max_amount": 0.5,
-               "min_amount": 0.1,
-               "price": 230.55,
-               "max_volume": 115.28,
-               "min_volume": 23.06,
-               "order_requirements_fullfilled": True,
-               "trading_partner_information": {
-                   "username": "bla",
-                   "is_kyc_full": True,
-                   "trust_level": "gold",
-                   "bank_name": "Sparkasse",
-                   "bic": "HASPDEHHXXX",
-                   "seat_of_bank": "DE",
-                   "rating": 99,
-                   "amount_trades": 52
-               },
-               "order_requirements": {
-                   "min_trust_level": "gold",
-                   "only_kyc_full": True,
-                   "seat_of_bank": [
-                       "DE",
-                       "NL"
-                   ],
-                   "payment_option": 1,
-               }
-           },
-           "errors": [
-
-           ],
-           "credits": 12
-        }
+        response = self.sampleData('showOrderbook_buy')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showOrderbook(params.get('type'),
                                 params.get('trading_pair'),
@@ -186,9 +138,7 @@ class TestBtcdeAPIDocu(TestCase):
                            params.get('price'),
                            params.get('trading_pair'),
                            params.get('type'))
-        response = {"order_id": "A1234BC",
-                    "errors": [],
-                    "credits": 8}
+        response = self.sampleData('createOrder')
         m.post(requests_mock.ANY, json=response, status_code=201)
         self.conn.createOrder(params.get('type'),
                               params.get('trading_pair'), params.get('max_amount'),
@@ -204,8 +154,7 @@ class TestBtcdeAPIDocu(TestCase):
         base_url = 'https://api.bitcoin.de/v2/orders'
         url_args = '/{}/{}'.format(params.get('order_id'),
                                    params.get('trading_pair'))
-        response = {"errors": [],
-                    "credits": 5}
+        response = self.sampleData('deleteOrder')
         m.delete(requests_mock.ANY, json=response, status_code=200)
         self.conn.deleteOrder(params.get('order_id'),
                               params.get('trading_pair'))
@@ -223,37 +172,7 @@ class TestBtcdeAPIDocu(TestCase):
                    .format(params.get('price'),
                            params.get('trading_pair'),
                            params.get('type'))
-        response = {"orders":
-                    [
-                     {
-                       "order_id": "2EDYNS",
-                       "trading_pair": "btceur",
-                       "type": "sell",
-                       "max_amount": 0.5,
-                       "min_amount": 0.2,
-                       "price": 250.55,
-                       "max_volume": 125.28,
-                       "min_volume": 50.11,
-                       "end_datetime": "2015-01-20T15:00:00+02:00",
-                       "new_order_for_remaining_amount": True,
-                       "state": 0,
-                       "order_requirements":
-                       {
-                          "min_trust_level": "silver",
-                          "only_kyc_full": True,
-                          "payment_option": 1,
-                          "seat_of_bank": "NL"
-                       },
-                       "created_at": "2015-01-10T15:00:00+02:00",
-                      },
-                    ],
-                    "page": {
-                        "current": 1,
-                        "last": 2
-                    },
-                    "errors": [],
-                    "credits": 15
-                    }
+        response = self.sampleData('showMyOrders')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showMyOrders(type=params.get('type'),
                                trading_pair=params.get('trading_pair'),
@@ -268,31 +187,7 @@ class TestBtcdeAPIDocu(TestCase):
         base_url = 'https://api.bitcoin.de/v2/orders/{}'\
                    .format(params.get('order_id'))
         url_args = ''
-        response = {"order":
-                    {
-                        "order_id": "2EDYNS",
-                        "trading_pair": "btceur",
-                        "type": "sell",
-                        "max_amount": 0.5,
-                        "min_amount": 0.2,
-                        "price": 250.55,
-                        "max_volume": 125.28,
-                        "min_volume": 50.11,
-                        "end_datetime": "2015-01-20T15:00:00+02:00",
-                        "new_order_for_remaining_amount": True,
-                        "state": 0,
-                        "order_requirements":
-                        {
-                            "min_trust_level": "silver",
-                            "only_kyc_full": True,
-                            "payment_option": 1,
-                            "seat_of_bank": "DE"
-                        },
-                        "created_at": "2015-01-10T15:00:00+02:00"
-                     },
-                    "errors": [],
-                    "credits": 15
-                    }
+        response = self.sampleData('showMyOrderDetails')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showMyOrderDetails(params.get('order_id'))
         history = m.request_history
@@ -312,8 +207,8 @@ class TestBtcdeAPIDocu(TestCase):
                            params.get('order_id'),
                            params.get('trading_pair'),
                            params.get('type'))
-        response = {"errors": [],
-                    "credits": 8}
+        response = self.sampleData('executeTrade')
+        m.get(requests_mock.ANY, json=response, status_code=200)
         m.post(requests_mock.ANY, json=response, status_code=201)
         self.conn.executeTrade(params.get('order_id'),
                                params.get('type'),
@@ -327,31 +222,7 @@ class TestBtcdeAPIDocu(TestCase):
         '''Test the function showMyTrades.'''
         base_url = 'https://api.bitcoin.de/v2/trades'
         url_args = ''
-        response = {"order":
-                    {
-                        "order_id": "2EDYNS",
-                        "trading_pair": "btceur",
-                        "type": "sell",
-                        "max_amount": 0.5,
-                        "min_amount": 0.2,
-                        "price": 250.55,
-                        "max_volume": 125.28,
-                        "min_volume": 50.11,
-                        "end_datetime": "2015-01-20T15:00:00+02:00",
-                        "new_order_for_remaining_amount": True,
-                        "state": 0,
-                        "order_requirements":
-                        {
-                           "min_trust_level": "silver",
-                           "only_kyc_full": True,
-                           "payment_option": 1,
-                           "seat_of_bank": "DE"
-                        },
-                        "created_at": "2015-01-10T15:00:00+02:00"
-                    },
-                    "errors": [],
-                    "credits": 15
-                    }
+        response = self.sampleData('showMyTrades')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showMyTrades()
         history = m.request_history
@@ -366,31 +237,7 @@ class TestBtcdeAPIDocu(TestCase):
         url_args = '?trading_pairs={}&type={}'\
                    .format(params.get('trading_pairs'),
                            params.get('type'))
-        response = {"order":
-                    {
-                        "order_id": "2EDYNS",
-                        "trading_pair": "btceur",
-                        "type": "sell",
-                        "max_amount": 0.5,
-                        "min_amount": 0.2,
-                        "price": 250.55,
-                        "max_volume": 125.28,
-                        "min_volume": 50.11,
-                        "end_datetime": "2015-01-20T15:00:00+02:00",
-                        "new_order_for_remaining_amount": True,
-                        "state": 0,
-                        "order_requirements":
-                        {
-                           "min_trust_level": "silver",
-                           "only_kyc_full": True,
-                           "payment_option": 1,
-                           "seat_of_bank": "DE"
-                        },
-                        "created_at": "2015-01-10T15:00:00+02:00"
-                    },
-                    "errors": [],
-                    "credits": 15
-                    }
+        response = self.sampleData('showMyTrades')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showMyTrades(type=params.get('type'),
                                trading_pairs=params.get('trading_pairs'))
@@ -403,39 +250,7 @@ class TestBtcdeAPIDocu(TestCase):
         params = {'trade_id': '1337'}
         base_url = 'https://api.bitcoin.de/v2/trades'
         url_args = '/{}'.format(params.get('trade_id'))
-        response = {"trade":
-                    {
-                        "trade_id": "2EDYNS",
-                        "trading_pair": "btceur",
-                        "type": "sell",
-                        "amount": 0.5,
-                        "price": 250.55,
-                        "volume": 125.28,
-                        "fee_eur": 0.6,
-                        "fee_btc": 0.0025,
-                        "fee_currency": 0.0025,
-                        "new_trade_id_for_remaining_amount": "C4Y8HD",
-                        "state": 1,
-                        "my_rating_for_trading_partner": "positive",
-                        "trading_partner_information":
-                        {
-                           "username": "testuser",
-                           "is_kyc_full": True,
-                           "bank_name": "sparkasse",
-                           "bic": "HASPDEHHXXX",
-                           "rating": 99,
-                           "amount_trades": 42,
-                           "trust_level": "gold",
-                           "seat_of_bank": "DE",
-                        },
-                        "payment_method": 1,
-                        "created_at": "2015-01-10T15:00:00+02:00",
-                        "successfully_finished_at":
-                        "2015-01-10T15:00:00+02:00",
-                    },
-                    "errors": [],
-                    "credits": 15,
-                    }
+        response = self.sampleData('showMyTradeDetails')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showMyTradeDetails(params.get('trade_id'))
         history = m.request_history
@@ -446,8 +261,7 @@ class TestBtcdeAPIDocu(TestCase):
         '''Test the function showAccountInfo.'''
         base_url = 'https://api.bitcoin.de/v2/account'
         url_args = ''
-        response = {"errors": [],
-                    "credits": 5}
+        response = self.sampleData('showAccountInfo')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showAccountInfo()
         history = m.request_history
@@ -459,8 +273,7 @@ class TestBtcdeAPIDocu(TestCase):
         params = {'trading_pair': 'btceur'}
         base_url = 'https://api.bitcoin.de/v2/orders/compact'
         url_args = '?trading_pair={}'.format(params.get('trading_pair'))
-        response = {"errors": [],
-                    "credits": 5}
+        response = self.sampleData('showOrderbookCompact')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showOrderbookCompact(params.get('trading_pair'))
         history = m.request_history
@@ -472,24 +285,7 @@ class TestBtcdeAPIDocu(TestCase):
         params = {'trading_pair': 'btceur'}
         base_url = 'https://api.bitcoin.de/v2/trades/history'
         url_args = '?trading_pair={}'.format(params.get('trading_pair'))
-        response = {"trading_pair": "btceur",
-                    "trades": [
-                         {
-                             "date": 1435922625,
-                             "price": 230,
-                             "amount": "2.50000000",
-                             "tid": 1252020
-                         },
-                         {
-                             "date": 1435922655,
-                             "price": 200.1,
-                             "amount": "0.60000000",
-                             "tid": 1252023
-                         }
-                     ],
-                    "errors": [],
-                    "credits": 19
-                    }
+        response = self.sampleData('showPublicTradeHistory')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showPublicTradeHistory(params.get('trading_pair'))
         history = m.request_history
@@ -501,15 +297,7 @@ class TestBtcdeAPIDocu(TestCase):
         params = {'trading_pair': 'btceur'}
         base_url = 'https://api.bitcoin.de/v2/rates'
         url_args = '?trading_pair={}'.format(params.get('trading_pair'))
-        response = {"trading_pair": "btceur",
-                    "rates": {
-                           "rate_weighted": "257.3999269",
-                           "rate_weighted_3h": "258.93994247",
-                           "rate_weighted_12h": "255.30363219"
-                      },
-                    "errors": [],
-                    "credits": 19
-                    }
+        response = self.sampleData('showRates')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showRates(params.get('trading_pair'))
         history = m.request_history
@@ -521,15 +309,7 @@ class TestBtcdeAPIDocu(TestCase):
         params = {'currency': 'btceur'}
         base_url = 'https://api.bitcoin.de/v2/account/ledger'
         url_args = '?currency={}'.format(params.get('currency'))
-        response = {"trading_pair": "btceur",
-                    "rates": {
-                           "rate_weighted": "257.3999269",
-                           "rate_weighted_3h": "258.93994247",
-                           "rate_weighted_12h": "255.30363219"
-                      },
-                    "errors": [],
-                    "credits": 19
-                    }
+        response = self.sampleData('showAccountLedger')
         m.get(requests_mock.ANY, json=response, status_code=200)
         self.conn.showAccountLedger(params.get('currency'))
         history = m.request_history
