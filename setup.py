a31b9b3fba55fdf0504cb8757d71c9dc66ba3e76
peshay <peshay@users.noreply.github.com>
2018-02-07 13:51:35 +0100

    2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """Setup to install Bitcoin.de API Python Module."""
 from distutils.core import setup
 setup(name='btcde',
-      version='2.2',
+      version='2.3',
       py_modules=['btcde'],
       install_requires=['requests', 'future'],
       description='API Wrapper for Bitcoin.de Trading API.',
