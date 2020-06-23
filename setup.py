fca216ca4ff016c5b875836d71b5827fcf8d3a4f
peshay <anhubert@gmail.com>
2020-06-23 14:49:06 +0200

    build: :white_check_mark: added tests for more recent python versions

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(name='btcde',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6', ],
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.8', ],
       keywords='bitcoin.de bitcoin btc api',
 
       )
